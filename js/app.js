cafffaa1344215626d2329145872ff4fdbe0e141
Tam Pham <tam.pham@sutrixmedia.com>
2014-12-24 13:09:11 +0700

    udpate fake user data

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var staffs = [
     email: 'tampham47@live.com',
     joinedDate: '01/09/2014',
     avatar: 'tampham47.jpg',
-    department: 'Frontend'
+    department: 'Front-end'
   },
   {
     id: '2',
@@ -25,7 +25,163 @@ var staffs = [
     email: 'soanguyen69@live.com',
     joinedDate: '01/09/2014',
     avatar: 'soanguyen69.jpg',
-    department: 'Frontend'
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Tran Khac Anh Duc',
+    dateOfBirth: '1990-04-17',
+    gender: 'Male',
+    address: '10 Tran Quoc Toan, P7, Q3, TPHCM',
+    id: '37112319',
+    mobile: '0123-456-789',
+    skype: 'sutrix.duc.tran',
+    email: 'duc.tran@sutrixmedia.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Pham Quoc Vinh',
+    dateOfBirth: '1987-12-12',
+    gender: 'Female',
+    address: '23 Le Quy Don- Ha Noi',
+    id: '37112239',
+    mobile: '0123-456-312',
+    skype: 'sutrix.vinh.quoc',
+    email: 'vinh.quoc@sutrixmedia.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Nguyen Duc Chung',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: '23 Le Quy Don- Ha Dong',
+    id: '37134339',
+    mobile: '0123-456-322',
+    skype: 'chungCr',
+    email: 'chungcr@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Le Minh Chien',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: 'Viet Tri- Phu Tho',
+    id: '54112419',
+    mobile: '0123-123-412',
+    skype: 'leminh.xu',
+    email: 'leminh.xu.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Nguyen Le Hoai Nam',
+    dateOfBirth: '1990-03-23',
+    gender: 'Female',
+    address: 'Thanh Pho Ha Noi',
+    id: '54112411',
+    mobile: '0123-123-411',
+    skype: 'namhoai.le',
+    email: 'namle.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Dinh Van Duc',
+    dateOfBirth: '1990-03-23',
+    gender: 'Female',
+    address: 'TP Thai Binh- Thai Binh',
+    id: '54112412',
+    mobile: '0123-123-413',
+    skype: 'dinhvan.duc',
+    email: 'dinhvan.duc.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Le Dinh Cuong',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: 'Ha Dong',
+    id: '54112413',
+    mobile: '0123-123-412',
+    skype: 'cuongle',
+    email: 'cuongle.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Phan Dinh Phuong',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: 'TP Ha Tinh, Ha Tinh',
+    id: '54112414',
+    mobile: '0123-123-232',
+    skype: 'phuongphan.hut',
+    email: 'phuongphan.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Pham Minh Tien',
+    dateOfBirth: '1990-03-23',
+    gender: 'Female',
+    address: 'Duy Nhat - Thai Binh',
+    id: '54112416',
+    mobile: '0123-153-412',
+    skype: 'minhtien.hut',
+    email: 'minhtien.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Nguyen Tien Minh',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: 'Ky Anh - Ha Tinh',
+    id: '54112417',
+    mobile: '0123-123-412',
+    skype: 'tienminh.hut',
+    email: 'tienminh.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Nguyen Nhat Minh',
+    dateOfBirth: '1990-03-23',
+    gender: 'Male',
+    address: 'Ha Dong',
+    id: '54112418',
+    mobile: '0123-123-412',
+    skype: 'leminh.xu',
+    email: 'leminh.xu.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
+  },
+  {
+    fullName: 'Do Van Kien',
+    dateOfBirth: '1990-03-23',
+    gender: 'Female',
+    address: 'TP Thanh Hoa- Thanh Hoa',
+    id: '54112419',
+    mobile: '0123-123-412',
+    skype: 'kiendo.hut',
+    email: 'kiendo.hut@gmail.com',
+    joinedDate: '2014-10-10',
+    avatar: 'assets/images/image.jp',
+    department: 'Front-end'
   }
 ];
 
@@ -33,16 +189,9 @@ var comments = [
   {
     id: '1',
     creatorId: '',
-    userId: '1',
+    userId: '37112319',
     dateCreated: '19/12/2014',
     comment: 'Thằng này khá'
-  },
-  {
-    id: '2',
-    creatorId: '',
-    userId: '2',
-    dateCreated: '19/12/2014',
-    comment: 'Con này được'
   }
 ];
 
@@ -152,6 +301,20 @@ App.Router.map(function() {
   this.resource('staffs.new');
 });
 
+App.DashboardRoute = Ember.Route.extend({
+  model: function(){
+    var model = [
+      {Frontend: 400},
+      {PHP: 252},
+      {Java: 172},
+      {QC: 286},
+      {Mobile: 207},
+      {Admin: 32}
+    ];
+    return model;
+  }
+});
+
 App.StaffsRoute = Ember.Route.extend({
   model: function() {
     return blStaff.getAll();
@@ -201,7 +364,7 @@ App.IndexController = Ember.ObjectController.extend({
     login: function() {
       var userName = this.get('userName'),
         password = this.get('password');
-      if ((userName != 'soanguyen69') & (password != 'hellongaymoi'))
+      if ((userName != 'soanguyen69') || (password != 'hellongaymoi'))
         alert('Please login with soanguyen69/hellongaymoi!');
       else
         this.transitionToRoute('dashboard');
